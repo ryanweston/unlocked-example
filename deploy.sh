db32ad82c0383b3311a4149da27293141e4853ed
Ryan Weston <ryanlweston@gmail.com>
2022-03-19 15:30:40 +0000

    chore(app): update unlocked + change deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ryanweston/unlocked-example.git main:gh-pages
+git push -f git@github.com:ryanweston/unlocked-website.git main:gh-pages
 
 cd -
