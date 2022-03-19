d0e15d070e028a87b89dcfb7c45f67dd51822b63
Ryan Weston <ryanlweston@gmail.com>
2022-03-19 14:50:03 +0000

    change to main

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ryanweston/unlocked-example.git master:gh-pages
+git push -f git@github.com:ryanweston/unlocked-example.git main:gh-pages
 
 cd -
