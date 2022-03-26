b3a42d07621cfd394a80cbd2724549b09801adbb
Ryan Weston <ryanlweston@gmail.com>
2022-03-26 15:54:21 +0000

    feat(app): begin looking at desing tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 // const defaultTheme = require('tailwindcss/defaultTheme')
 // const unlockedTailwind = require('@unlocked/foundation/tailwind.config.js')
 
+const tokens = require('./src/tokens/tokens');
+
+const colors = Object.fromEntries(Object
+  .values(tokens.color)
+  .map(({ attributes, value }) => [
+    attributes.type, value
+  ]));
+
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ['./index.html', './node_modules/@unlocked/foundation/dist/**/**/**/*.{js,css}','./src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    // colors: {
-    //   'blue': '#000000',
-    //   'black': '#000000'
-    // },
+    colors,
   //  ...unlockedTailwind.theme,
     fontFamily: {
       sans: ['"Inter var"'],
