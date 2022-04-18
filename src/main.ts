ea6e15a2a992838a99359f21bdb87fdb06880cd5
Ryan Weston <ryanlweston@gmail.com>
2022-04-18 13:33:03 +0100

    chore(app): misc updates + small fixes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import './tailwind.css'
 import { createRouter, createWebHistory } from 'vue-router'
-// @ts-expect-error fix types on
 import { defaultTheme, unlockedGlobal } from '@unlocked/foundation'
 import App from './App.vue'
 import { routes } from './routes'
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,15 @@ const tokens = require('@unlocked/foundation/tokens')
 
 // TODO: Look for a better way to handle Tailwind's purge
 
-// console.log({ ...tokens })
-
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ['./index.html', './node_modules/@unlocked/foundation/dist/**/**/**/*.{js,css}', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    ...tokens,
     fontFamily: {
       sans: ['"Inter var"'],
     },
     extend: {
-      colors: {
-        grey: '#ededed',
-        transparent: 'transparent',
-      },
+      ...tokens,
     },
   },
   plugins: [
