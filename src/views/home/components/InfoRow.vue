06aae849f637abe37bf1a305576e3035ca859454
Ryan Weston <ryanlweston@gmail.com>
2022-08-07 17:00:41 +0100

    chore: improve syntax

--- src/views/home/components/InfoRow.vue
+++ src/views/home/components/InfoRow.vue
@@ -5,16 +5,16 @@ export interface Item {
   content: string
 }
 
-const prop = defineProps<{ items: Item[] }>()
+const props = defineProps<{ items: Item[] }>()
 </script>
 
 <template>
   <div class="flex flex-col md:flex-row w-100 items-center justify-between mx-auto">
-    <div v-for="(item, i) in items" :key="i" :class="[i > 0 ? 'md:ml-10 ' : '', 'flex-column mt-10 lg:mt-0']">
+    <div v-for="(item, i) in props.items" :key="i" :class="[i > 0 ? 'md:ml-10 ' : '', 'flex-column mt-10 lg:mt-0']">
       <!-- Title -->
       <div class="flex flex-row items-center font-bold text-lg">
         <div v-html="item.icon" />
-        <u-headline :size="6" class="ml-2">
+        <u-headline :size="5" class="ml-3">
           {{ item.title }}
         </u-headline>
       </div>
